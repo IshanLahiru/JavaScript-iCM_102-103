724d9d439e0c6da9c7a78f96145837700740bbb4
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-08-22 21:04:50 +0530

    data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,1 +1,24 @@
-console.log("hellow");
+//Linking a javascript file
+
+
+////////////////////////////////////////////////
+// Values and Variables
+/*
+var firstName = `sanath`;
+let name = `malitha`;
+const pi = 3.14;
+
+console.log(name);
+*/
+////////////////////////////////////////////////
+// Data Types
+/*
+let variable = `kamal`;
+variable = false;
+variable = 10;
+variable = 10.2;
+variable = Symbol("foo");
+variable = {};
+console.log(typeof variable)
+*/
+
