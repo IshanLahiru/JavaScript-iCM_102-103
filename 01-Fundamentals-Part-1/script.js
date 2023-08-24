fcd21395f9b13811ea702f23016d73bb9fb3c654
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-08-24 22:10:32 +0530

    updated the data types section

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,34 +4,34 @@
 ////////////////////////////////////////////////
 // Values and Variables
 /*
-Values
-======
-In javaScript, values are the fundamental building blocks that data takes on.
-Values can be various types, such as numbers,strings,booleans,objects,arrays and more.
-Variables are used to store and manipulate these values.
- ex :
-    # Numbers: Numeric values, both integers and floating-point numbers. Example: 42, 3.14.
-    # Strings: Textual data enclosed in single ('') or double ("") quotes. Example: 'Hello, world!', "JavaScript".
-    # Booleans: Logical values representing true or false. Example: true, false.
-    # Null: A special value representing the absence of any value or object. Example: null.
-    # Undefined: A value given to uninitialized variables or unavailable properties. Example: undefined.
-    # Objects: Collections of key-value pairs, used to represent more complex data structures. Example: { key: 'value', age: 25 }.
-    # Arrays: Ordered lists of values, indexed by integers. Example: [1, 2, 3, 4].
-    # Functions: Blocks of reusable code that can be invoked with arguments. Example: function add(a, b) { return a + b; }.
+// Values
+// ======
+// In javaScript, values are the fundamental building blocks that data takes on.
+// Values can be various types, such as numbers,strings,booleans,objects,arrays and more.
+// Variables are used to store and manipulate these values.
+//  ex :
+//     # Numbers: Numeric values, both integers and floating-point numbers. Example: 42, 3.14.
+//     # Strings: Textual data enclosed in single ('') or double ("") quotes. Example: 'Hello, world!', "JavaScript".
+//     # Booleans: Logical values representing true or false. Example: true, false.
+//     # Null: A special value representing the absence of any value or object. Example: null.
+//     # Undefined: A value given to uninitialized variables or unavailable properties. Example: undefined.
+//     # Objects: Collections of key-value pairs, used to represent more complex data structures. Example: { key: 'value', age: 25 }.
+//     # Arrays: Ordered lists of values, indexed by integers. Example: [1, 2, 3, 4].
+//     # Functions: Blocks of reusable code that can be invoked with arguments. Example: function add(a, b) { return a + b; }.
 
 
-Variables
-=========
-A variable is a symbolic name that references a value in JavaScript. 
-You declare variables using the var, let, or const keyword, followed by the variable name.
+// Variables
+// =========
+// A variable is a symbolic name that references a value in JavaScript. 
+// You declare variables using the var, let, or const keyword, followed by the variable name.
 
-    # var: Used for declaring variables globally or within a function.
-    # let: Introduces block-scoped variables. It's the recommended way to declare variables that will change.
-    # const: Also block-scoped, but once assigned, the value cannot be changed. Used for constants.
+//    # var: Used for declaring variables globally or within a function.
+//    # let: Introduces block-scoped variables. It's the recommended way to declare variables that will change.
+//    # const: Also block-scoped, but once assigned, the value cannot be changed. Used for constants.
 
-    Example variable declarations:
-*/
-/*
+//    Example variable declarations:
+
+
     var firstName = `sanath`;
     let name = `malitha`;
     const pi = 3.14;
@@ -46,13 +46,38 @@ You declare variables using the var, let, or const keyword, followed by the vari
 ////////////////////////////////////////////////
 // Data Types
 /*
-let variable = `kamal`;
-variable = false;
-variable = 10;
-variable = 10.2;
-variable = Symbol("foo");
-variable = {};
-console.log(typeof variable)
+
+// JavaScript has several built-in data types that allow you to represent different kinds of values. 
+// Here are the main data types in JavaScript:
+
+// Primitive Data Types
+// ====================
+
+// These are basic data types that hold single values.
+
+//    # Number: Represents both integer and floating-point numbers. Example: 42, 3.14.
+//    # String: Represents sequences of characters. Example: 'Hello, world!', "JavaScript".
+//    # Boolean: Represents true or false values.
+//    # Null: Represents the intentional absence of any value. It's a value itself.
+//    # Undefined: Represents the value of uninitialized variables or undefined properties.
+//    # Symbol:  Represents a unique and immutable value. 
+
+// Non-primitive Data Types (Reference Types)
+// ==========================================
+
+// These are more complex data types that can hold collections of values and are referenced by a memory address.
+//
+//    # Object: Represents a collection of key-value pairs. Objects can be used to store and manipulate complex data structures.
+//    # Array: Represents an ordered list of values. Arrays are objects with numeric indices.
+
+    let variable = 10;//number
+    variable = 10.2;            //number
+    variable = `kamal`;         //string
+    variable = false;           //boolean
+    variable = Symbol("foo");   //symbol(ES6)
+    variable = {};              //object
+    variable = ["red","green"]; //array
+    console.log(typeof variable)
 */
 ////////////////////////////////////////////////
 // Basic operators
