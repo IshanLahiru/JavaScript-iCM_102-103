d9206be8565bd82df475f9ff0b21636560187fa3
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-08-24 21:32:37 +0530

    updated the values and variables section

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,12 +4,41 @@
 ////////////////////////////////////////////////
 // Values and Variables
 /*
-var firstName = `sanath`;
-let name = `malitha`;
-const pi = 3.14;
+Values
+======
+In javaScript, values are the fundamental building blocks that data takes on.
+Values can be various types, such as numbers,strings,booleans,objects,arrays and more.
+Variables are used to store and manipulate these values.
+ ex :
+    # Numbers: Numeric values, both integers and floating-point numbers. Example: 42, 3.14.
+    # Strings: Textual data enclosed in single ('') or double ("") quotes. Example: 'Hello, world!', "JavaScript".
+    # Booleans: Logical values representing true or false. Example: true, false.
+    # Null: A special value representing the absence of any value or object. Example: null.
+    # Undefined: A value given to uninitialized variables or unavailable properties. Example: undefined.
+    # Objects: Collections of key-value pairs, used to represent more complex data structures. Example: { key: 'value', age: 25 }.
+    # Arrays: Ordered lists of values, indexed by integers. Example: [1, 2, 3, 4].
+    # Functions: Blocks of reusable code that can be invoked with arguments. Example: function add(a, b) { return a + b; }.
 
-console.log(name);
+
+Variables
+=========
+A variable is a symbolic name that references a value in JavaScript. 
+You declare variables using the var, let, or const keyword, followed by the variable name.
+
+    # var: Used for declaring variables globally or within a function.
+    # let: Introduces block-scoped variables. It's the recommended way to declare variables that will change.
+    # const: Also block-scoped, but once assigned, the value cannot be changed. Used for constants.
+
+    Example variable declarations:
 */
+/*
+    var firstName = `sanath`;
+    let name = `malitha`;
+    const pi = 3.14;
+
+    console.log(name);
+*/
+
 ////////////////////////////////////////////////
 // Data Types
 /*
@@ -71,7 +100,7 @@ console.log(msg);
 */
 //////////////////////////////////////////////////
 //Flow controle (if/else Statements)
-
+/*
 let age = 15;
 
 if(age > 18) {
@@ -81,3 +110,4 @@ if(age > 18) {
 }else{
     console.log(`You can't get the driving lisance.`);
 }
+*/
