e5fc0f2b4f57aa2ffdfeefa5d79ad88c86d156fc
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-08-28 20:39:19 +0530

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,8 @@
+'use strict';
+//////////////////////////////////////////////////////////
+//Activating Strict Mode
+
+// let isAdded = false;
+// const pass = true;
+// if(pass)isAdde = true;
+// if (isAdded) console.log(`It's added.`);
