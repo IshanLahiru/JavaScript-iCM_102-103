b6b1f31a5f2c844985a957b229a00a4fa174435c
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-08-28 20:57:48 +0530

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -6,3 +6,36 @@
 // const pass = true;
 // if(pass)isAdde = true;
 // if (isAdded) console.log(`It's added.`);
+
+////////////////////////////////////////////////////////////
+//Functions
+
+function logger(){
+    console.log(`logger`);
+}
+logger();
+
+function numberLogger(num1, num2){
+    console.log(num1);
+    console.log(num2);
+}
+numberLogger(2,5);
+
+function sumOf(num1,num2){
+    return num1+num2;
+}
+let x = sumOf(5,4);
+
+console.log(`the sum is ${x}.`);
+
+const fullName = function(fName,sName){
+    console.log(`${fName} ${sName}.`);
+}
+fullName("Ishan", "Lahiru");
+
+const theName = (fName,sName) => {
+    console.log(`${fName} ${sName} 22.`);
+}
+theName("Ishan", "Lahiru");
+
+isPrime(2);
