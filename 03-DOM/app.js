4aaf7f7d051f0cadac520ef029ceb3d095de5b7e
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-09-04 21:27:28 +0530

    seletors and etc..

---
+++ 03-DOM/app.js
@@ -0,0 +1,44 @@
+// console.log(document.getElementById('heading'));
+
+// const doc = document.body;
+
+// doc.append('hellow');
+
+// const h1 = document.getElementById('heading');
+// console.log(h1);
+
+// console.log();
+
+// h1.innerText = 'hellow';
+
+// const bgl = document.getElementsByClassName('container');
+// console.log(bgl);
+
+// const tg = document.getElementsByTagName('div');
+// console.log(tg);
+
+// console.log(document.querySelector('#heading'));
+
+
+
+// let headings = document.querySelectorAll('#heading');
+
+// headings.forEach(element => {
+//     element.innerText = 'bye';    
+// });
+
+// let h1 = document.querySelector('#heading');
+// h1.innerText = "kamal";
+
+// // h1.setAttribute("class","btn btn-primary");
+// // console.log(h1.getAttribute("class"));
+
+// h1.classList.add("btn","btn-primary");
+// h1.classList.remove("btn-primary");
+
+
+
+//document.querySelectorAll();
+
+
+
