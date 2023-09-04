7a6fa49bc74a1818edb6c595692c340dabecee81
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-09-04 23:02:46 +0530

    to do list

--- 03-DOM/app.js
+++ 03-DOM/app.js
@@ -1,60 +1,41 @@
-// // console.log(document.getElementById('heading'));
+document.addEventListener("DOMContentLoaded",function(){
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
 
-// // const doc = document.body;
+    addTaskButton.addEventListener("click", function(){
+        const taskText = taskInput.value.trim();
 
-// // doc.append('hellow');
+        if(taskText === ""){
+            alert("Please enter a task.");
+            return;
+        }
 
-// // const h1 = document.getElementById('heading');
-// // console.log(h1);
+        const taskItem = document.createElement('li');
+        taskItem.className = "list-group-item";
+        taskItem.innerHTML = `
+            <span>${taskText}</span>
+            <button class="btn btn-danger btn-sm float-right delete-task">Delete</button>          
+        
+        `;
+        const deleteButton = taskItem.querySelector(".delete-task");
+        deleteButton.addEventListener("click", function(){
+            taskItem.remove();
+        });
 
-// // console.log();
+        console.log(taskItem);
 
-// // h1.innerText = 'hellow';
-
-// // const bgl = document.getElementsByClassName('container');
-// // console.log(bgl);
-
-// // const tg = document.getElementsByTagName('div');
-// // console.log(tg);
-
-// // console.log(document.querySelector('#heading'));
-
-
-
-// // let headings = document.querySelectorAll('#heading');
-
-// // headings.forEach(element => {
-// //     element.innerText = 'bye';    
-// // });
-
-//  let h1 = document.querySelector('#heading');
-//  h1.innerText = "kamal";
-
-// h1.setAttribute("class","btn btn-primary");
-// console.log(h1.getAttribute("class"));
-
-// h1.classList.add("btn","btn-primary");
-// h1.classList.remove("btn-primary");
-// console.log(h1);
-
-// h1.style.fontSize = "50px";
-// h1.style.backgroundColor = "green";
-// h1.style.color = "yellow";
-// h1.style["color"] = "blue";
-
-// let comp = document.createElement("h1");
-// comp.innerText = "hellow";
-// console.log(comp);
-// let div = document.querySelector('.container');
-// console.log(div);
-
-// div.insertBefore(comp,div.children[3]);
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+        taskInput.addEventListener("keypress",function(event){
+            if(event.key === "Enter"){
+                addTaskButton.click();
+            }
+        });
 
 
-// h1.addEventListener("mouseover", function(){
-//     alert("mouse on kamal");
-// });
 
-// //div.appendChild(comp);
+    });
+});
 
 
