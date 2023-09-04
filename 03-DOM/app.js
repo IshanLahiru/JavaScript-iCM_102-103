71a831cd9afb620ddfbc031ea2524487eb60eb40
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-09-04 22:03:50 +0530

    more

--- 03-DOM/app.js
+++ 03-DOM/app.js
@@ -1,44 +1,60 @@
-// console.log(document.getElementById('heading'));
+// // console.log(document.getElementById('heading'));
 
-// const doc = document.body;
+// // const doc = document.body;
 
-// doc.append('hellow');
+// // doc.append('hellow');
 
-// const h1 = document.getElementById('heading');
-// console.log(h1);
+// // const h1 = document.getElementById('heading');
+// // console.log(h1);
 
-// console.log();
+// // console.log();
 
-// h1.innerText = 'hellow';
+// // h1.innerText = 'hellow';
 
-// const bgl = document.getElementsByClassName('container');
-// console.log(bgl);
+// // const bgl = document.getElementsByClassName('container');
+// // console.log(bgl);
 
-// const tg = document.getElementsByTagName('div');
-// console.log(tg);
+// // const tg = document.getElementsByTagName('div');
+// // console.log(tg);
 
-// console.log(document.querySelector('#heading'));
+// // console.log(document.querySelector('#heading'));
 
 
 
-// let headings = document.querySelectorAll('#heading');
+// // let headings = document.querySelectorAll('#heading');
 
-// headings.forEach(element => {
-//     element.innerText = 'bye';    
-// });
+// // headings.forEach(element => {
+// //     element.innerText = 'bye';    
+// // });
 
-// let h1 = document.querySelector('#heading');
-// h1.innerText = "kamal";
+//  let h1 = document.querySelector('#heading');
+//  h1.innerText = "kamal";
 
-// // h1.setAttribute("class","btn btn-primary");
-// // console.log(h1.getAttribute("class"));
+// h1.setAttribute("class","btn btn-primary");
+// console.log(h1.getAttribute("class"));
 
 // h1.classList.add("btn","btn-primary");
 // h1.classList.remove("btn-primary");
+// console.log(h1);
+
+// h1.style.fontSize = "50px";
+// h1.style.backgroundColor = "green";
+// h1.style.color = "yellow";
+// h1.style["color"] = "blue";
+
+// let comp = document.createElement("h1");
+// comp.innerText = "hellow";
+// console.log(comp);
+// let div = document.querySelector('.container');
+// console.log(div);
+
+// div.insertBefore(comp,div.children[3]);
 
 
+// h1.addEventListener("mouseover", function(){
+//     alert("mouse on kamal");
+// });
 
-//document.querySelectorAll();
-
+// //div.appendChild(comp);
 
 
