2b3aea7f3a6ef564013c46c7e93364baf25b0bf7
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-09-08 21:42:44 +0530

    perform actions

---
+++ 04-jQuery/app.js
@@ -0,0 +1,49 @@
+//document.addEventListener("DOMContentLoaded" ,function(){
+// $(document).ready(function(){
+// console.log($('h1').text());
+// });
+
+/////////////////////////////////////////////
+
+// // Select by tag name
+// let h1Tags = jQuery('h1');
+// console.log(h1Tags);
+
+// let btns = $('button');
+// console.log(btns);
+
+// // Select by class
+// let sbc = $('.btn-danger');
+// console.log(sbc);
+
+// // Select by id
+// let sbi = $('#sv-btn');
+// console.log(sbi);
+
+/////////////////////
+
+// let sp = $('button.btn#sv-btn');
+// console.log(sp);
+
+// console.log($('button').not('.btn-danger'));
+
+// console.log($('button').filter(':odd'));
+
+///////////////////////////////////////////////
+//perform actions
+//////////////////////////////////////////////
+
+// //set text content
+// $('h1').text('India');
+
+// //get text content
+// console.log($('h1').text());
+
+// //get HTML content
+// console.log($('body').html());
+
+// //set HTML content
+// $('body').html('<p>hellow</p>');
+
+////////////////////////////////////////////////
+
